e5339a6d902ab89cfb746c326de488f64aee02e6
povilassta <48487014+povilassta@users.noreply.github.com>
2020-03-23 16:09:35 +0200

    Add files via upload

---
+++ TCP-Client/cmake-build-debug/CMakeFiles/TCP_Client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TCP_Client.dir/main.c.o"
+  "TCP_Client.exe"
+  "TCP_Client.pdb"
+  "libTCP_Client.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TCP_Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TCP-Server/cmake-build-debug/CMakeFiles/TCP_Server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TCP_Server.dir/main.c.o"
+  "TCP_Server.exe"
+  "TCP_Server.pdb"
+  "libTCP_Server.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TCP_Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
